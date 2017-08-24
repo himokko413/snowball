153ff8c2b879051c91a29c60227f26d8fa208d69
princessnadia <princess.nadia.1019@gmail.com>
2017-08-24 08:06:57 -0400

    initial commit, i swear

---
+++ main.py
@@ -0,0 +1,79 @@
+clientkey = 'MzUwMDY2Nzc4NzQ5MTQwOTkz.DH-ooQ.wuaPe62i2-m7R23HqDRttvXSIYo'
+
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('?hello'):
+        msg = 'Ni hao, {0.author.mention}!'.format(message)
+    elif message.content.startswith('?healing'):
+        msg = 'Stop being a Genji, {0.author.mention}!'.format(message)
+        await bot.send_message(message.channel, msg)
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('{0.subcommand_passed} doesn\'t know what cool is'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    """Is the bot cool?"""
+    await bot.say('Yes, the bot is cool. Maybe even cold!')
+
+bot.run(clientkey)
